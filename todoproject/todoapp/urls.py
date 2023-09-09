35016f2154710df9cfd36878323ded3a706d5bf5
tomoki0v0 <uragunb653@gmail.com>
2023-09-09 12:01:32 +0900

    9/9 12:00

--- todoproject/todoapp/urls.py
+++ todoproject/todoapp/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
-from .views import TaskList
+from .views import TaskList, TaskDetail, TaskCreate,TaskUpdate,TaskDelete, TaskListLoginView
 
 urlpatterns = [
-    path("", TaskList.as_view(), name="object_lists")
+    path("", TaskList.as_view(), name="tasks"),
+    path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
+    path("create-task", TaskCreate.as_view(), name="create-task"),
+    path("edit-task/<int:pk>/", TaskUpdate.as_view(), name="edit-task"),
+    path("delete-task/<int:pk>/", TaskDelete.as_view(), name="delete-task"),
+    path("login/", TaskListLoginView.as_view(), name="login")
 ]
--- todoproject/todoapp/views.py
+++ todoproject/todoapp/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.contrib.auth.views import LoginView
 
 from todoapp.models import Task
 
@@ -8,6 +13,36 @@ class TaskList(ListView):
     model = Task
     context_object_name = "tasks"
 
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task"
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__" # ["user", "title", ....]
+    success_url = reverse_lazy("tasks")
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = "__all__" # ["user", "title", ....]
+    success_url = reverse_lazy("tasks")
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = "__all__" # ["user", "title", ....]
+    success_url = reverse_lazy("tasks")
+
+class TaskDelete(DeleteView):
+    model = Task
+    fields = "__all__" # ["user", "title", ....]
+    success_url = reverse_lazy("tasks")
+    context_object_name = "task"
+
+class TaskListLoginView(LoginView):
+    fields = "__all__"
+
+    def get_success_url(self):
+        return reverse_lazy("tasks")
 
 
 """
